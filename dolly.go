db162cd67fd323831cc3be79bb742ef3217e0b33
Jorge <jorgeluisrezende94@gmail.com>
2018-02-21 12:22:20 -0300

    project refactoring and thinking in the features that it will have

--- dolly.go
+++ dolly.go
@@ -1,43 +1,43 @@
 package main
+
 import (
-  "os/exec"
-  "fmt"
-  "strconv"
-  "log"
-  "io/ioutil"
-  "net/http"
-  "github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-////////////////////////////////////////////////////////////////////////////////
-func CreateServer(p *int, h *string){
-  routes := mux.NewRouter().StrictSlash(true)
-  routes.HandleFunc("/"+*h, deployhook)
-  http.ListenAndServe(":"+strconv.Itoa(*p), routes)
-  log.Println("listening on {*p}")
+// CreateServer - it set up the web server to liten the hook
+func CreateServer(p *int, h *string) {
+	routes := mux.NewRouter().StrictSlash(true)
+	routes.HandleFunc("/"+*h, deployhook)
+	http.ListenAndServe(":"+strconv.Itoa(*p), routes)
+	log.Println("listening on {*p}")
 }
 
-func deployhook(w http.ResponseWriter, r *http.Request){
-  gitPullCmd()
+func deployhook(w http.ResponseWriter, r *http.Request) {
+	gitPullCmd()
 }
 
-func gitPullCmd(){
-   Out, Err := exec.Command("git", "pull", "origin", "master").Output()
+func gitPullCmd() {
+	Out, Err := exec.Command("git", "pull", "origin", "master").Output()
 
-   if Err != nil{
-    
-    s := fmt.Sprint(Err) + ": Out - Error on Pull"
-    writeFile(s+"\n", "error")
+	if Err != nil {
 
-   }else{
+		s := fmt.Sprint(Err) + ": Out - Error on Pull"
+		writeFile(s+"\n", "error")
 
-     s:= string(Out)
-     writeFile(s+"\n", "success")     
-   }
+	} else {
+		s := string(Out)
+		writeFile(s+"\n", "success")
+	}
 }
 
-func writeFile(in string, logName string){
-  fout := []byte(in);
-  ioutil.WriteFile("./"+logName+"-logs.txt", fout, 0644)
+func writeFile(in string, logName string) {
+	fout := []byte(in)
+	ioutil.WriteFile("./"+logName+"-logs.txt", fout, 0644)
 }
-
--- main.go
+++ main.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "crypto/md5"
-  "encoding/hex"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
 )
+
 func main() {
-  FlagsUsing()
+	FlagsUsing()
 }
 
-func FlagsUsing(){
-  var start = flag.Bool("s", false, "It starts the server listener")
-  var port = flag.Int("p", 8001, "It sets a webserver listener port")
-  var hash = flag.String("hash", "", "It sets the hash that you got in -gh (You can put in it another hash that you want)")
-  var gHash = flag.String("gh", "", "It gives you a md5 string hash that will be the endpoint to your hook")
-  flag.Parse()
+// FlagsUsing - it is listening the flags
+func FlagsUsing() {
+	var start = flag.Bool("s", false, "It starts the server listener")
+	var port = flag.Int("p", 8001, "It sets a webserver listener port")
+	var hash = flag.String("hash", "", "It sets the hash that you got in -gh (You can put in it another hash that you want)")
+	var gHash = flag.String("gh", "", "It gives you a md5 string hash that will be the endpoint to your hook")
+	flag.Parse()
 
-  if *start {
+	if *start {
 
-     CreateServer(port, hash)
+		CreateServer(port, hash)
 
-  }
-  if *gHash != ""{
+	}
+	if *gHash != "" {
 
-    fmt.Println(generateHash(*gHash) + " - put it in the hash flag to get up your webhook server")
+		fmt.Println(generateHash(*gHash) + " - put it in the hash flag to get up your webhook server")
 
-  }
+	}
 }
 
-func generateHash (text string) string{
-   hasher := md5.New()
-   hasher.Write([]byte(text))
-   return hex.EncodeToString(hasher.Sum(nil))
+func generateHash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
